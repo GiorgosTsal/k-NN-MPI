7dd6872bec33640329b257e270b29b0045d69b29
gtsal <tsalgiwrgos@gmail.com>
2019-11-17 22:49:22 +0200

    some basics for initializing

---
+++ inc/knn.h
@@ -0,0 +1,28 @@
+// Definition of the kNN result struct
+typedef struct knnresult {
+    int* nidx;		//!< Indices (0-based) of nearest neighbors [m - by - k]
+    double* ndist;	//!< Distance of nearest neighbors			[m - by - k]
+    int m;			//!< Number of query points					[scalar]
+    int k;			//!< Number of nearest neighbors			[scalar]
+} knnresult;
+
+    
+    
+    
+//! Compute k nearest neighbors of each point in X [n-by-d]
+/*!
+	\param	X		Corpus data points			[n-by-d]
+	\param	Y		Query data points			[m-by-d]
+	\param	n		Number of corpus points		[scalar]
+	\param	m		Number of query points		[scalar]
+	\param	d		Number of dimensions		[scalar]
+	\param	k		Number of neighbors			[scalar]
+
+	\return The kNN result
+*/
+knnresult kNN(double* X, double* Y, int n, int m, int d, int k);
+
+void populateArray(double *array, int n, int d);
+void printArray(double *array, int n, int d);
+void printIndicesArray(int * array, int size);
+int * indexArray(int size);
