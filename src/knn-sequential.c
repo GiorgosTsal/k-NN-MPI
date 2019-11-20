38b5ea4065f148b512c2fa06ec21b85a150d75bc
gtsal <tsalgiwrgos@gmail.com>
2019-11-20 12:18:11 +0200

    find k distances working good. need to return the indexes too/make a struct or sth

--- src/knn-sequential.c
+++ src/knn-sequential.c
@@ -23,11 +23,12 @@ void printArray(double *array, int n, int d){
     for (int i = 0; i < n; ++i)
     {
        // cout << i << ": ";
-        for (int j = 0; j < d; ++j)
+        for (int j = 0; j < d; ++j){
             printf("%f\n", *(array + i*d + j));
 			putchar('\n');
 			fflush(stdout);
-    }
+		}
+	}
 }
 //Print Indices of an array
 void printIndicesArray(int *array, int size){
@@ -44,8 +45,17 @@ int * indexArray(int size){
 	}
 	return idxOfarray;
 }
-//custom swap 
-void swap(double *p, double *q){
+
+//custom swap for doubles
+void swap_d(double *p, double *q){
+    double tmp;
+
+    tmp = *p;
+    *p = *q;
+    *q = tmp;
+}
+//custom swap for doubles
+void swap_i(int *p, int *q){
     double tmp;
 
     tmp = *p;
@@ -56,23 +66,25 @@ void swap(double *p, double *q){
 // It considers the last element as pivot
 // and moves all smaller element to left of
 // it and greater elements to right
-int partition(double arr[], int l, int r){
-    double x = arr[r];
-    int i = l;
-    for (int j = l; j <= r - 1; j++) {
-        if (*(arr +j) <= x) {
-            swap(arr+i, arr +j); 
-            i++;
-        }
-    }
-    swap(arr + i, arr + r); 
-    return i;
+int partition(double *arr, int *idx,int l, int r){
+	double x = arr[r];
+	int i = l;
+	for (int j = l; j <= r - 1; j++) {
+		if (*(arr +j) <= x) {
+			swap_d(arr+i, arr +j); 
+			swap_i(idx+i, idx +j); 
+			i++;
+		}
+	}
+	swap_d(arr + i, arr + r); 
+	swap_i(idx+i, idx +r);
+	return i;
 }
 // This function returns k'th smallest
 // element in arr[l..r] using QuickSort
 // based method.  ASSUMPTION: ALL ELEMENTS
 // IN ARR[] ARE DISTINCT
-double kthSmallest(double arr[], int l, int r, int k){
+double kthSmallest(double *arr, int *idx,int l, int r, int k){
     // If k is smaller than number of
     // elements in array
     if (k > 0 && k <= r - l + 1) {
@@ -80,19 +92,19 @@ double kthSmallest(double arr[], int l, int r, int k){
         // Partition the array around last
         // element and get position of pivot
         // element in sorted array
-        int index = partition(arr, l, r);
+        int index = partition(arr, idx, l, r);
 
         // If position is same as k
         if (index - l == k - 1)
-            return arr[index];
+            return *(arr + index);
 
         // If position is more, recur
         // for left subarray
         if (index - l > k - 1)
-            return kthSmallest(arr, l, index - 1, k);
+            return kthSmallest(arr, idx,l, index - 1, k);
 
         // Else recur for right subarray
-        return kthSmallest(arr, index + 1, r,
+        return kthSmallest(arr, idx,index + 1, r,
             k - index + l - 1);
     }
     // If k is more than number of
@@ -113,21 +125,22 @@ double kthSmallest(double arr[], int l, int r, int k){
 */
 knnresult kNN(double* X, double* Y, int n, int m, int d, int k){
 
+	knnresult knnres;
+	
 	double * distance = (double *)malloc(m * n  *sizeof(double));
-	 
+	double * Tdistance = (double *)malloc(m * n  *sizeof(double));
 	double *xRow = (double *)malloc(m * n  *sizeof(double));
 	double *yRow = (double *)malloc(m * sizeof(double));
 	int *indexes = (int*)malloc(m * n  *sizeof(int));
+
 	
 
 	for(int i=0; i<m; i++){
 		for(int j=0; j<n; j++){
 			*(indexes+i*n+j)=j;
-			printf("indexes: %d \n", *(indexes+i*n+j));
+			printf("indexes1: %d \n", *(indexes+i*n+j));
 		}
 	}
-	
-	
 	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<d; j++){
@@ -143,6 +156,7 @@ knnresult kNN(double* X, double* Y, int n, int m, int d, int k){
 	}
 	printf("yRow: \n");
 	printArray(yRow, m, 1);
+	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<m; j++){
 			*(distance + i*m + j) += xRow[i] + yRow[j];
@@ -151,26 +165,43 @@ knnresult kNN(double* X, double* Y, int n, int m, int d, int k){
 		}
 		
 	}
+	
+	
+	
 	//	After that, only keep the k shortest distances and the indices of the corresponding points (using k-select like in the
 	//	previous assignment?).
-	printf("Distances: \n");
-	printArray(distance, m, n);
 
+	
+	printf("\n=== Distances === \n" );
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			printf("%f " , *(distance + i*m + j));
+		}
+	printf("\n" );
+	}
+	
+	printf("\n" );
 	double *kNeighbours = (double *)malloc(k * sizeof(double));
 	for(int i=1; i<=k; i++){
-		printf("oi %d neibhor ine: %f ",i , kthSmallest(distance, 0, m-1, i));
-		*(kNeighbours + i) = kthSmallest(distance, 0, m-1, i);
+	
+		printf("oi %d neibhor ine: %f ",i , kthSmallest(distance, indexes,0, m*n-1, i));
+		//loipon eho teleiosei nomizo me to distances alla des to k esu ligo genika dn eimai 100% sigouros oti einai sosto
+		//i kthSmallest pou eho gyrnaei double ta k mikrotera distances edo. autos thelei na gurname k ta index
+		//https://www.geeksforgeeks.org/how-to-return-multiple-values-from-a-function-in-c-or-cpp/
+		//me tis texnikes edo logika pame sto na epistrefei domi i an eheis esu kamia kaluteri lusi ennoeite
+		//kai nomizo etsi teleionei to seiriako alla ksanaleo dn eimai sigouros oti ta apotelesmata pou vgazo einai sosta
+		//genika an einai check mia ton tropo se sxesi me ton typo D pou dinei gia na vroume tis apostaseis k an pisteueis oti einai sostos apla krata ta indexes k teleiose to seiriako
+		*(kNeighbours + i) = kthSmallest(distance, indexes, 0, m-1, i); //edo allaksa tin kthSmallest(prosthesa os parametro to indexes-pou tha einai ta index tou pinaka distance 
+																		//k ekana k mia swap gia integers pou tha xreiastei sigoura an pame me auti ti logiki)
+		
 		printf("\n");
 		//na to valo na gyrnaei k ta indices tous-kamia domi gia to dist??
 	}
 	
-	
-		
-	
 
 	
-   
 }
+
 
 
 
--- src/main.c
+++ src/main.c
@@ -5,25 +5,23 @@
 
 
 
-const int n = 3, d = 4, m = 3, k=2;
+const int n = 3, d = 4, m = 3, k=3;
 
 
 // Driver code 
 int main() 
 { 
 	
-	//double *X = (double *)malloc(n * d * sizeof(double));
-//	double *Y = (double *)malloc(m * d * sizeof(double));
-	//populateArray(X, n, d);
-	double X[3][4] = {{1000,1,1,1}, {3,3,3,3}, {6,6,6,6}};
+	double *X = (double *)malloc(n * d * sizeof(double));
+	double *Y = (double *)malloc(m * d * sizeof(double));
+	populateArray(X, n, d);
 	printf("Array X is: ");
 	printf("\n");
 	printArray(X, n, d);
 	printf("\n");
 	printf("Array Y is: ");
 	printf("\n");
-//	populateArray(Y, m, d);
-	double Y[3][4] = {{0,1,2,3}, {8,8,4,8}, {11,9,9,9}};
+	populateArray(Y, m, d);
 	printArray(Y, m, d);
 
 	int *idxX = (int *)malloc(n *sizeof(int));
