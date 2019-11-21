c061345ab9e56d2eaea6a19ad741ff33b8bb7178
gtsal <tsalgiwrgos@gmail.com>
2019-11-21 10:06:56 +0200

    measure time taken addition

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ const int n = 3, d = 4, m = 3, k=3;
 // Driver code 
 int main() 
 { 
+
 	
 	double *X = (double *)malloc(n * d * sizeof(double));
 	double *Y = (double *)malloc(m * d * sizeof(double));
@@ -33,12 +34,18 @@ int main()
 
 
 	printf("begin \n");
+	double  total_time;
+	struct timeval start, end;
+	gettimeofday (&start, NULL);
     knnresult result;
 	result = kNN(X, Y, n, m, d, k);
+	gettimeofday (&end, NULL);
+	total_time = (double)((end.tv_usec - start.tv_usec)/1.0e6 + end.tv_sec - start.tv_sec);
+	printf("Time taken by kNN function: : %f sec\n",total_time);
 	
+
 	printf("end \n");
    
-
     return 0; 
 } 
 
